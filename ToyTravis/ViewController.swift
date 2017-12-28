8221cb57e9681baf722ad40756faa5f78ce4dcdf
Faiz Mokhtar <mfmokhtar@gmail.com>
2017-12-28 10:57:13 +0800

    Initial Commit

---
+++ ToyTravis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToyTravis
+//
+//  Created by Faiz Mokhtar on 28/12/2017.
+//  Copyright © 2017 Faiz Mokhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
